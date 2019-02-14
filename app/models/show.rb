6353fa3f0ba39507edfc7c9bc985c8392a1f0595
Lauren Pardee <lap5151@gmail.com>
2019-02-14 01:47:24 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -8,4 +8,12 @@ class Show < ActiveRecord::Base
     Show.find_by(self.highest_rating)
   end
 
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.find_by(self.lowest_rating)
+  end
+
 end
